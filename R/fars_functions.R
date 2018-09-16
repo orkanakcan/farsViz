611714eb96845a125aa772f0ce93a16b097d5409
Orkan Akcan <orkan.akcan@gmail.com>
2018-09-16 12:26:39 +0800

    2018-09-16: Attempt #6 - resolve the global var. for n()

--- R/fars_functions.R
+++ R/fars_functions.R
@@ -128,7 +128,7 @@ fars_summarize_years <- function(years) {
   dat_list <- fars_read_years(years)
   dplyr::bind_rows(dat_list) %>%
     dplyr::group_by(~year, ~MONTH) %>%
-    dplyr::summarize(n = n()) %>%
+    dplyr::summarize_(.dots = setNames(~n(), "n")) %>%
     tidyr::spread(~year, ~n)
 }
 
